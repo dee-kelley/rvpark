d85ef9ccfd9fdb2ac8e1b875a48f3977e2a6b9ed
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2019-05-20 16:31:11 -0400

    still working on it

--- robotObj.js
+++ robotObj.js
@@ -7,19 +7,28 @@ const robotType = [{
     AERONAUTICAL: 'Aeronautical'
   }];
 
-    let name = '';
+    let name = document.getElementById("name").value;
     let selectedType = '';
     function setName(userInputName) {
       name = userInputName;
     }
-
+    console.log(name);
     function selectRobotTypeOption(index) {
       selectedType = robotType[index];
     }
+    console.log(`${name} NUMBER 2`);
 
-    function submit () {
+    function submit() {
+        console.log(`${name} NUMBER 2.5`)
         let robot = {};
-        robot.name = name;
+        robot.id = name;
         robot.type = selectedType;
-        return robot; 
+        console.log(`${name} NUMBER 3`)
+        // return robot; onclick="name=this.value"
+      }
+     
+      function handleClick() {
+        alert("Favorite weird creature: " + getRadioButtonValue(document.aye.whichThing));
+        //event.preventDefault(); // disable normal form submit behavior
+        return false; // prevent further bubbling of event
       }
