b5b57d30793aa4bcc819add69466bc9ac8bdbe30
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2019-05-21 10:55:04 -0400

    message2

--- robots.js
+++ robots.js
@@ -15,7 +15,7 @@ console.log(naming)
 const create = ()=>{
     const selectedType = document.getElementById("selectedType")
     document.getElementById("type").value = selectedType.options[selectedType.selectedIndex].text;
-    console.log(type)
+    console.log(selectedType)
 };
 
 $('.clickBait').on('click', (e)=> { 
@@ -26,6 +26,46 @@ $('.clickBait').on('click', (e)=> {
     $('.nameS').append(`hello ${input}, type ${inputTwo}`)
     return;
 });
+
+const tasks=[
+    {
+      description: 'do the dishes',
+      eta: 1000,
+    },{
+      description: 'sweep the house',
+      eta: 3000,
+    },{
+      description: 'do the laundry',
+      eta: 10000,
+    },{
+      description: 'take out the recycling',
+      eta: 4000,
+    },{
+      description: 'make a sammich',
+      eta: 7000,
+    },{
+      description: 'mow the lawn',
+      eta: 20000,
+    },{
+      description: 'rake the leaves',
+      eta: 18000,
+    },{
+      description: 'give the dog a bath',
+      eta: 14500,
+    },{
+      description: 'bake some cookies',
+      eta: 8000,
+    },{
+      description: 'wash the car',
+      eta: 20000,
+    },
+  ];
+
+  const doTask=()=>{
+        const selectedTask = document.getElementById("selectedTask")
+        document.getElementById("task").value = selectedTask.options[selectedTask.selectedIndex].text;
+        console.log(selectedTask);
+    };  
 
 /*[{ 
     UNIPEDAL: 'Unipedal',
